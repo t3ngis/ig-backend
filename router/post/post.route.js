ffdc2a6c288ebfe7c5bb0a02784e0f06ad18d06e
25HP3004 25HP3004 <25HP3004@admins-iMac.local>
2025-10-28 18:38:15 +0800

    new

--- router/post/post.route.js
+++ router/post/post.route.js
@@ -1,11 +1,11 @@
-import express from 'express'
-import { getPosts } from '../../controller/post/get-posts.js'
-import { CreatePost } from '../../controller/post/create-posts.js'
-import { authMiddleware } from '../../middleWare/authMiddleware.js'
+import express from "express";
+import { getPosts } from "../../controller/post/get-posts.js";
+import { CreatePost } from "../../controller/post/create-posts.js";
+import { authMiddleware } from "../../middleWare/authMiddleware.js";
 
-const postRouter = express.Router()
+const postRouter = express.Router();
 
-postRouter.get("/getPosts", authMiddleware, getPosts)
-postRouter.post("/create", authMiddleware, CreatePost)
+postRouter.get("/getPosts", authMiddleware, getPosts);
+postRouter.post("/createPost", authMiddleware, CreatePost);
 
-export default postRouter
+export default postRouter;
